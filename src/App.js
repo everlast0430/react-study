b6478c7df4fb387def2d96d49198fba695748c32
sabrikong <sabrikong@gmail.com>
2024-10-15 22:16:07 +0900

    first upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPageComponent from "./main/index";
+
+function App() {
+    return (
+        <div className="App">
+            <MainPageComponent/>
+        </div>
+    );
+}
+
+export default App;
