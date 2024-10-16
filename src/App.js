d7284237f9cd6c2478447a195e1f831f1b3abf7d
sabrikong <sabrikong@gmail.com>
2024-10-16 17:56:50 +0900

    git react router dom 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import MainPageComponent from "./main/index";
+import {Switch, Route} from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
     return (
         <div className="App">
-            <MainPageComponent/>
+            <Switch>
+                <Route exact={true} path={"/"}>
+                    <MainPageComponent/>
+                </Route>
+                <Route exact={true} path={"/upload"}>
+                    <UploadPage/>
+                </Route>
+                <Route exact={true} path={"/product"}>
+                    <ProductPage/>
+                </Route>
+            </Switch>
         </div>
     );
 }
